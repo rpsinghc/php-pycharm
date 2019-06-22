1e85a003c9c7734a9ee14c7453046da32a309c6c
Ram Prakash Singh <rpsinghc@gmail.com>
2019-06-22 15:49:14 +0530

    moving to github

---
+++ hello/Hello.py
@@ -0,0 +1,12 @@
+import sys
+import io
+
+
+def hello():
+    print "Im am hello"
+
+
+print(sys.getdefaultencoding())
+print ("Hello")
+print(sys.api_version)
+hello()
---
+++ helloenv/Hello.py
@@ -0,0 +1,5 @@
+import sys
+
+print(sys.getdefaultencoding())
+print ("Hello")
+print(sys.api_version)
